4b5ad7a5c8f3af5da1f53cf12d305008dd554696
shayrg <Shay@Shays-MacBook-Pro.local>
2017-04-28 10:06:32 -0400

    Initial commit

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"encoding/json"
+	"github.com/gorilla/mux"
+	//"path/filepath"
+	//"os"
+)
+
+func main() {
+	//Make router
+	router := mux.NewRouter().StrictSlash(true)
+	//Routs
+	router.HandleFunc("/", Index)
+	router.HandleFunc("/songs", Songs)
+	router.HandleFunc("/songs/{songId}", GetSongById)
+	router.HandleFunc("/getpic", GetPic)
+	router.HandleFunc("/getsong/{song}", GetSong)
+	router.HandleFunc("/getstyle", GetStyle)
+	//Listen and serve
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./index.html")
+}
+func Songs(w http.ResponseWriter, r *http.Request) {
+	mysongs := SongsStruct{
+		SongStruct{Name: "Help", Length: "3 Min", Order: "2",
+			Location: "getsong/song2"},
+		SongStruct{Name: "Steve", Length: "2 Min", Order: "2",
+			Location: "getsong/song1"},
+	}
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(mysongs)
+}
+func GetSongById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	songId := vars["songId"]
+	mystring := "Your song id is " + songId
+	fmt.Fprintln(w, mystring)
+}
+func GetPic(w http.ResponseWriter, r *http.Request) {
+	//http.FileServer(http.Dir("./music/pic.jpg"))
+	http.ServeFile(w, r, "./music/pic.jpg")
+}
+func GetSong(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	song := vars["song"]
+	http.ServeFile(w, r, "./music/"+song+".mp3")
+}
+func GetStyle(w http.ResponseWriter, r *http.Request){
+	http.ServeFile(w, r, "./style.css")
+}
---
+++ song.go
@@ -0,0 +1,10 @@
+package main
+
+type SongStruct struct {
+	Name     string `json:"name"`
+	Length   string `json:"length"`
+	Location string `json:"location"`
+	Order    string `json:"order"`
+}
+
+type SongsStruct []SongStruct
